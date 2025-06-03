ec359ba4c485fc42bf2c7adcc4ee6c62e13cc03e
LaibaKosar144 <laibakosar144@gmail.com>
2025-06-03 11:47:49 +0500

    refactor : convert to functional react component

--- final-exam/src/TrafficLightChanger.jsx
+++ final-exam/src/TrafficLightChanger.jsx
@@ -1,13 +1,49 @@
 import React from 'react'
-import './TrafficLightChanger.css'
+import './TrafficLightColors.css'
+import { useState, useEffect } from 'react'
 
 const TrafficLightChanger = () => {
+
+    {/*writing the useState and useEffect logic to change colors */}
+      
+
+
+
+    const {color,setColor} = useState('Red');
+
+     {/*initially putting red as our state value because simulation starts with red */}
+
+    useEffect(()=> {
+        {/*this logis is necessary to make a certain light run for some specific seconds, then return its value so new one could be updated accordingly */}
+        const currentColor = setInterval(() => {
+        setColor(Color => {
+        if (Color === 'Red') {
+          return 'Green'; {/*changing red to green as shown in simulation */}
+        } else if (prevLight === 'Green') {
+          return 'Yellow'; 
+        } else { 
+          return 'Red'; 
+        }
+      });
+    }, 2000);
+
+     return () => clearInterval(intervalId);
+  }, []);
+
+
+
+
+
+   
   return (
     <div>
+         <div> <h1> Traffic Light Simulator </h1></div>
+        {/*1 div inside which we will have 3 lights, using static css file we created */}
+        {/*providing css properties to each file */}
         <div className='Outer-Box'> 
-            <div className='red-light'></div>
-            <div className='yellow-light' ></div>
-            <div className='green-light'></div>
+            <div className='red-light'> RED</div>
+            <div className='yellow-light' > YELLOW</div>
+            <div className='green-light'> GREEN </div>
         </div> 
       
     </div>
@@ -15,3 +51,5 @@ const TrafficLightChanger = () => {
 }
 
 export default TrafficLightChanger
+
+
