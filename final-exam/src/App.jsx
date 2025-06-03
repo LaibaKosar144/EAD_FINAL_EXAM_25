66daa1ff1ed6056171a6a50812d05ba7f27a3dca
LaibaKosar144 <laibakosar144@gmail.com>
2025-06-03 11:07:36 +0500

    made css file named TrafficLightChanger but still havent described the css class properties yet

---
+++ final-exam/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import TrafficLightChanger from './TrafficLightChanger'
+
+function App() {
+  
+
+  return (
+    <>
+      <TrafficLightChanger />
+    </>
+  )
+}
+
+export default App
---
+++ final-exam/src/TrafficLightChanger.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './TrafficLightChanger.css'
+
+const TrafficLightChanger = () => {
+  return (
+    <div>
+        <div className='Outer-Box'> 
+            <div className='red-light'></div>
+            <div className='yellow-light' ></div>
+            <div className='green-light'></div>
+        </div> 
+      
+    </div>
+  )
+}
+
+export default TrafficLightChanger
